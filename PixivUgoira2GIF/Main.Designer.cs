53f61921294d1ba8a7a91d13e744efa900b6e3d9
HY <yuakezero@gmail.com>
2014-08-04 21:07:27 +0800

    First Version

---
+++ PixivUgoira2GIF/Main.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Ugoira2GIF
+{
+    partial class Ugoira2GIF
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ugoira2GIF));
+            this.label1 = new System.Windows.Forms.Label();
+            this.loop = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 70);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "請拖入zip文件";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // loop
+            // 
+            this.loop.AutoSize = true;
+            this.loop.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.loop.Checked = true;
+            this.loop.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.loop.Location = new System.Drawing.Point(14, 85);
+            this.loop.Name = "loop";
+            this.loop.Size = new System.Drawing.Size(60, 16);
+            this.loop.TabIndex = 1;
+            this.loop.Text = "循環：";
+            this.loop.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.loop.UseVisualStyleBackColor = true;
+            this.loop.CheckedChanged += new System.EventHandler(this.loop_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "延遲：     ms";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(29, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "100";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "壓縮：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "5",
+            "10",
+            "15",
+            "20"});
+            this.comboBox1.Location = new System.Drawing.Point(237, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(45, 20);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Text = "10";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // Ugoira2GIF
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 113);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.loop);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Ugoira2GIF";
+            this.Text = "Ugoira2GIF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox loop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ PixivUgoira2GIF/Main.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gif.Components;
+using System.IO;
+using System.Threading;
+
+namespace Ugoira2GIF
+{
+    public partial class Ugoira2GIF : Form
+    {
+        int repeat = 0;
+        int qua = 10;
+        int delay = 100;
+        public Ugoira2GIF()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Move;
+            else e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            Thread workThread = new Thread(delegate()
+            {
+                string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+                string dir = System.IO.Path.GetDirectoryName(path);//目錄名稱
+                string ext = System.IO.Path.GetExtension(path);
+                string nam = System.IO.Path.GetFileNameWithoutExtension(path);
+                if (ext != ".zip")
+                {
+                    MessageBox.Show("請拖入zip格式檔案");
+                }
+                else
+                {
+
+                    this.label1.Text = "正在解壓";
+                    System.Threading.Thread.Sleep(500);
+                    ZIP.UnpackFiles(path, dir + "/" + nam);
+                    this.label1.Text = "解壓完畢";
+                    System.Threading.Thread.Sleep(500);
+                    this.label1.Text = "正在生成GIF";
+                    System.Threading.Thread.Sleep(500);
+                    try
+                    {
+                        string[] jpgImgNames = Directory.GetFiles(dir + "/" + nam); // 获取各帧图片的文件名
+                        string dtStr = DateTime.Now.ToString("yyyyMMddhhmmss");
+                        string outPutPath = string.Format(dir + "/" + nam + ".gif", dtStr); // 生成要保存成的gif图片的文件名
+                        // 创建动画（gif）
+                        AnimatedGifEncoder animate = new AnimatedGifEncoder();
+                        animate.Start(outPutPath);
+                        animate.SetDelay(delay);
+                        animate.SetRepeat(repeat); // -1：不重复，0：无限循环
+                        animate.SetQuality(qua);
+                        int filecount=0;
+                        foreach (var item in jpgImgNames)
+                        {
+                            filecount++;
+                            Image imgFrame = Image.FromFile(item);
+                            this.label1.Text = "正在添加幀：\r\n" + System.IO.Path.GetFileNameWithoutExtension(item) + "\r\n" + filecount.ToString() + "/" + jpgImgNames.Length.ToString();
+                            animate.AddFrame(imgFrame); // 添加帧
+                            imgFrame.Dispose();
+                        }
+                        animate.Finish();
+                        this.label1.Text = "生成成功";
+
+                    }
+                    catch (Exception ex)
+                    {
+                        this.label1.Text = "出錯啦：" + ex.Message;
+                    }
+                    try
+                    {
+                        DirectoryInfo di = new DirectoryInfo(dir + "/" + nam);
+                        di.Delete(true);
+                    }
+                    catch (Exception ex)
+                    {
+                        this.label1.Text = "出錯啦：" + ex.Message;
+                    }
+                }
+            });
+            workThread.Start();
+        }
+
+        private void loop_CheckedChanged(object sender, EventArgs e)
+        {
+            if (loop.Checked)
+            {
+                repeat = 0;
+            }
+            else {
+                repeat = -1;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                delay = int.Parse(textBox1.Text);
+            }
+            catch {
+                textBox1.Text = "100";
+                delay = 100;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                qua = int.Parse(comboBox1.Text);
+            }
+            catch
+            {
+                comboBox1.Text = "10";
+                qua = 10;
+            }
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                qua = int.Parse(comboBox1.Text);
+            }
+            catch
+            {
+                comboBox1.Text = "10";
+                qua = 10;
+            }
+        }
+    }
+}
